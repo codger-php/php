b6bed3afc1beea9841e519fe185d7f6bb8eec54e
Marijn Ophorst <marijn@monomelodies.nl>
2018-09-08 18:12:38 +0200

    add (most of) methods needed so far

--- src/Klass.php
+++ src/Klass.php
@@ -2,15 +2,103 @@
 
 namespace Codger\Php;
 
-use Codger\Generate\Recipe;
-use Twig_Environment;
-use Twig_Loader_Filesystem;
-
 class Klass extends Recipe
 {
+    protected $template = 'class.html.twig';
+
     public function __construct()
     {
-        parent::__construct(new Twig_Environment(new Twig_Loader_Filesystem(dirname(__DIR__).'/templates')));
+        parent::__construct();
+        $this->variables->variables = [];
+        $this->variables->methods = [];
+    }
+
+    public function setNamespace(string $namespace) : Klass
+    {
+        $this->variables->namespace = $namespace;
+        return $this;
+    }
+
+    public function usesNamespaces(string ...$namespaces) : Klass
+    {
+        $this->variables->uses_namespaces = $namespaces;
+        return $this;
+    }
+
+    public function setName(string $name) : Klass
+    {
+        $this->variables->name = $name;
+        return $this;
+    }
+
+    public function isFinal(bool $final = true) : Klass
+    {
+        $this->variables->final = $final;
+        return $this;
+    }
+
+    public function isAbstract(bool $abstract = true) : Klass
+    {
+        $this->variables->abstract = $abstract;
+        return $this;
+    }
+
+    public function extendsClass(string $class) : Klass
+    {
+        $this->variables->extends = $class;
+        return $this;
+    }
+
+    public function implementsInterfaces(string ...$interfaces) : Klass
+    {
+        $this->variables->implements = implode(', ', $interfaces);
+        return $this;
+    }
+
+    public function usesTraits(string ...$traits) : Klass
+    {
+        $this->variables->uses_traits = $traits;
+        return $this;
+    }
+
+    public function definesConstants(array $constants) : Klass
+    {
+        foreach ($constants as &$constant) {
+            $constant = $this->quote($constant);
+        }
+        $this->variables->constants = $constants;
+        return $this;
+    }
+
+    public function defineVariable(string $name, string $value = null, string $visibility = 'public') : Klass
+    {
+        $value = $this->quote($value);
+        $this->variables->variables[] = compact('name', 'visibility', 'value');
+        return $this;
+    }
+
+    public function addMethod($name, callable $callback) : Klass
+    {
+        $method = new Method($name, $this->twig);
+        $callback($method);
+        $this->variables->methods[] = $method->render();
+        return $this;
+    }
+
+    public function render() : string
+    {
+        return preg_replace("@\n}\n$@m", "}\n", parent::render());
+    }
+
+    protected function quote(string $unquoted = null) : string
+    {
+        if (is_null($unquoted)) {
+            return 'NULL';
+        }
+        if (is_numeric($unquoted)) {
+            return (string)$unquoted;
+        }
+        return "'".str_replace("'", "\'", $unquoted)."'";
     }
 }
 
