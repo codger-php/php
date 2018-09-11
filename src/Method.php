fbde28957dc4efeb0b75fd5e6888e0f0769d410d
Marijn Ophorst <marijn@sensimedia.nl>
2018-09-11 11:35:50 +0200

    add some missing methods

--- src/Method.php
+++ src/Method.php
@@ -3,16 +3,56 @@
 namespace Codger\Php;
 
 use Twig_Environment;
+use DomainException;
 
 class Method extends Recipe
 {
     protected $template = 'method.html.twig';
+    protected $arguments = [];
 
-    public function __construct(string $name, Twig_Environment $twig = null)
+    public function __construct(string $name, Twig_Environment $twig = null, callable $declaration = null)
     {
         parent::__construct($twig);
         $this->variables->name = $name;
         $this->variables->visibility = 'public';
+        if (isset($declaration)) {
+        }
+    }
+
+    public function setVisibility(string $visibility) : Method
+    {
+        if (!in_array($visibility, ['public', 'protected', 'private'])) {
+            throw new DomainException("Visibility must be one of public, protected or private, not $visibility.");
+        }
+        $this->variables->visibility = $visibility;
+        return $this;
+    }
+
+    public function isPrivate() : Method
+    {
+        return $this->setVisibility('private');
+    }
+
+    public function isProtected() : Method
+    {
+        return $this->setVisibility('protected');
+    }
+
+    public function isPublic() : Method
+    {
+        return $this->setVisibility('public');
+    }
+
+    public function isFinal(bool $status = true) : Method
+    {
+        $this->variables->final = $status;
+        return $this;
+    }
+
+    public function isAbstract(bool $status = true) : Method
+    {
+        $this->variables->abstract = $status;
+        return $this;
     }
 
     public function setBody(string $body) : Method
@@ -23,6 +63,12 @@ class Method extends Recipe
             $line = "        $line";
         }
         $this->variables->body = implode("\n", $lines);
+        return $this;
+    }
+
+    public function addArgument(Argument $argument) : Method
+    {
+        $this->arguments[] = $argument;
         return $this;
     }
 }
