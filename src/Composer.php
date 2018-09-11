238f29ec901b7208d30096009de1b8dbd56f7c7b
Marijn Ophorst <marijn@monomelodies.nl>
2018-09-11 15:03:02 +0200

    setup composer helper, very first version

---
+++ src/Composer.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Codger\Php;
+
+class Composer
+{
+    private $composer;
+
+    public function __construct(string $path = null)
+    {
+        $path = $path ?? getcwd();
+        $this->composer = json_decode(file_get_contents("$path/composer.json"));
+    }
+
+    public function hasDependency(string $name) : bool
+    {
+        return array_key_exists($name, $this->composer->dependencies);
+    }
+
+    public function addDependency(string $name) : void
+    {
+        exec("composer require $name");
+    }
+}
+
