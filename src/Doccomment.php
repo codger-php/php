955906e4ac8e1dd5439183fcdd9e990db63e575b
Marijn Ophorst <marijn@monomelodies.nl>
2018-09-12 17:02:42 +0200

    add doccomment trait

---
+++ src/Doccomment.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Codger\Php;
+
+trait Doccomment
+{
+    public function addDoccomment(string $comment) : Recipe
+    {
+        $lines = explode("\n", $comment);
+        if (count($lines) > 1) {
+            array_walk($lines, function (string &$line) {
+                $line = trim($line);
+                $line = " * $line";
+            }
+        } else {
+            $lines = ["/** {$lines[0]} */"];
+        }
+        return $this->set('doccomment', implode("\n", $lines));
+    }
+}
+
