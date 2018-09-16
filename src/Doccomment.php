4b3ad977c09107c83fca94e0755f486299a84959
Marijn Ophorst <marijn@monomelodies.nl>
2018-09-16 08:23:02 +0200

    fix typo and move to main recipe

--- src/Doccomment.php
+++ src/Doccomment.php
@@ -2,8 +2,6 @@
 
 namespace Codger\Php;
 
-use Twig_SimpleFilter;
-
 /**
  * `use` this trait for any recipe that needs to support doccomments.
  */
@@ -15,26 +13,7 @@ trait Doccomment
      */
     public function setDoccomment(string $comment) : Recipe
     {
-        static $inited = false;
-        if (!$inited) {
-            $this->twig->addFilter(new Twig_SimpleFilter('formatDoccomment', function (string $comment, bool $indent = true) : string {
-                $lines = explode("\n", $comment);
-                $indent = $indent ? '    ' : '';
-                if (count($lines) > 1) {
-                    array_walk($lines, function (string &$line) {
-                        $line = trim($line);
-                        $line = "$indent * $line";
-                    });
-                    array_unshift($lines, "$indent/**");
-                    $lines[] = "$indent */\n";
-                } else {
-                    $lines = ["$indent/** {$lines[0]} */\n"];
-                }
-                return implode("\n", $lines);
-            }));
-            $inited = true;
-        }
-        return $this->set('doccoment', $comment);
+        return $this->set('doccomment', $comment);
     }
 
     /**
