068c1122fc03bac6402ee55179bd3eb660218ca9
Marijn Ophorst <marijn@monomelodies.nl>
2018-09-16 08:23:13 +0200

    implement this here, and do it properly/flexibly

--- src/Recipe.php
+++ src/Recipe.php
@@ -4,6 +4,7 @@ namespace Codger\Php;
 
 use Twig_Environment;
 use Twig_Loader_Filesystem;
+use Twig_SimpleFilter;
 use Codger\Generate;
 
 abstract class Recipe extends Generate\Recipe
@@ -11,6 +12,21 @@ abstract class Recipe extends Generate\Recipe
     public function __construct()
     {
         parent::__construct(new Twig_Environment(new Twig_Loader_Filesystem(dirname(__DIR__).'/templates')));
+        $this->twig->addFilter(new Twig_SimpleFilter('formatDoccomment', function (string $comment, int $indent = 4) : string {
+            $lines = explode("\n", $comment);
+            $indent = str_repeat(' ', $indent);
+            if (count($lines) > 1) {
+                array_walk($lines, function (string &$line) use ($indent) {
+                    $line = trim($line);
+                    $line = "$indent * $line";
+                });
+                array_unshift($lines, "$indent/**");
+                $lines[] = "$indent */\n";
+            } else {
+                $lines = ["$indent/** {$lines[0]} */\n"];
+            }
+            return implode("\n", $lines);
+        }));
     }
 }
 
