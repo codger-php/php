b0f6908444ee990ef67133b99da136222835e594
Marijn Ophorst <marijn@monomelodies.nl>
2018-09-16 08:12:58 +0200

    implement formatting as a filter (with optional indenting)

--- src/Doccomment.php
+++ src/Doccomment.php
@@ -2,27 +2,51 @@
 
 namespace Codger\Php;
 
+use Twig_SimpleFilter;
+
+/**
+ * `use` this trait for any recipe that needs to support doccomments.
+ */
 trait Doccomment
 {
+    /**
+     * @var string $comment
+     * @return Codger\Php\Recipe
+     */
     public function setDoccomment(string $comment) : Recipe
     {
-        $lines = explode("\n", $comment);
-        if (count($lines) > 1) {
-            array_walk($lines, function (string &$line) {
-                $line = trim($line);
-                $line = "     * $line";
-            });
-            array_unshift($lines, '    /**');
-            $lines[] = "     */\n";
-        } else {
-            $lines = ["    /** {$lines[0]} */\n"];
+        static $inited = false;
+        if (!$inited) {
+            $this->twig->addFiler(new Twig_SimpleFilter('formatDoccomment', function (string $comment, bool $indent = true) : string {
+                $lines = explode("\n", $comment);
+                $indent = $indent ? '    ' : '';
+                if (count($lines) > 1) {
+                    array_walk($lines, function (string &$line) {
+                        $line = trim($line);
+                        $line = "$indent * $line";
+                    });
+                    array_unshift($lines, "$indent/**");
+                    $lines[] = "$indent */\n";
+                } else {
+                    $lines = ["$indent/** {$lines[0]} */\n"];
+                }
+                return implode("\n", $lines);
+            }));
+            $inited = true;
         }
-        return $this->set('doccomment', implode("\n", $lines));
+        return $this->set('doccoment', $comment);
     }
 
+    /**
+     * Append a doccomment to an already existing one. This is useful if you
+     * need to add to auto-generated comments.
+     *
+     * @param string $comment
+     * @return Codger\Php\Recipe
+     */
     public function appendDoccomment(string $comment) : Recipe
     {
-        return $this->setDoccomment($this->get('doccomment')."\n\n$comment");
+        return $this->setDoccomment($this->get('doccomment').$comment);
     }
 }
 
