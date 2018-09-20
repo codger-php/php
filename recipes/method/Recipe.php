82e5f8d505b8b2e5fbc9f6832759a52b97ce1944
Danny van der Veldt <danny@sensimedia.nl>
2018-09-20 15:00:45 +0200

    Add test recipe for methods too

---
+++ recipes/method/Recipe.php
@@ -0,0 +1,27 @@
+<?php
+
+use Codger\Php\Method;
+use Codger\Php\Property;
+
+/** Example recipe for generating a PHP method */
+return function () : Method {
+    $twig = new Twig_Environment(new Twig_Loader_Filesystem(dirname(__DIR__).'/../templates'));
+    $recipe = new Method('testMe', $twig);
+    $recipe->isPrivate()
+        ->isStatic()
+        ->isFinal()
+        ->isAbstract()
+        ->setReturnType('string')
+        ->setNullable(true)
+        ->setBody(<<<EOT
+if ((1 + 1) === 2) {
+    return 'ok';
+} else {
+    return null;
+}
+EOT
+        )
+        ->output('php://stdout');
+    return $recipe;
+};
+
