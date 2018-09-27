469c35d6f3250cc062e8fa2623c8bd8fb9c8b647
Marijn Ophorst <marijn@monomelodies.nl>
2018-09-27 07:58:17 +0200

    fix typo

--- src/Composer.php
+++ src/Composer.php
@@ -15,7 +15,7 @@ class Composer
     public function hasDependency(string $name) : bool
     {
         return (isset($this->composer->require) && array_key_exists($name, $this->composer->require))
-            || (isset($this->composer->{'require-dev'} && array_key_exists($name, $this->composer->{'require-dev'}));
+            || (isset($this->composer->{'require-dev'}) && array_key_exists($name, $this->composer->{'require-dev'}));
     }
 
     public function addDependency(string $name) : void
