00a68b1a41f5736f361166747de59a83e479a440
Marijn Ophorst <marijn@monomelodies.nl>
2019-04-14 13:58:52 +0200

    helper generator for simple functions

---
+++ src/Funktion.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Codger\Php;
+
+use Twig_Environment;
+use DomainException;
+use ReflectionFunction;
+
+class Funktion extends Lambda
+{
+    use Doccomment;
+
+    /** @var string */
+    protected $template = 'function.html.twig';
+
+    /**
+     * @param string $name Name of the function
+     * @param Twig_Environment $twig Optional Twig environment
+     * @param callable $declaration Optional argument and return type
+     *  declaration closure (function can have empty body)
+     */
+    public function __construct(string $name, Twig_Environment $twig = null, callable $declaration = null)
+    {
+        parent::__construct($twig, $declaration);
+        $this->variables->name = $name;
+    }
+}
+
