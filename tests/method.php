525e1c44d1a25af0efa7e9c6a857dcac6d9d2032
Marijn Ophorst <marijn@monomelodies.nl>
2019-04-14 14:25:39 +0200

    fix tests and make check more strict

--- tests/method.php
+++ tests/method.php
@@ -13,73 +13,74 @@ return function () use ($twig, $method) : Generator {
     yield function () use ($method) {
         $method->setVisibility('private');
         $result = $method->render();
-        assert(strpos($result, 'private function login()'));
+        assert(strpos($result, 'private function login()') !== false);
         $method->setVisibility('protected');
         $result = $method->render();
-        assert(strpos($result, 'protected function login()'));
+        assert(strpos($result, 'protected function login()') !== false);
         $method->setVisibility('public');
         $result = $method->render();
-        assert(strpos($result, 'public function login()'));
+        assert(strpos($result, 'public function login()') !== false);
     };
     
     /** isPrivate sets the visibility to private */
     yield function () use ($method) {
         $method->isPrivate();
         $result = $method->render();
-        assert(strpos($result, 'private function login()'));
+        assert(strpos($result, 'private function login()') !== false);
     };
     
     /** isProtected sets the visibility to protected */
     yield function () use ($method) {
         $method->isProtected();
         $result = $method->render();
-        assert(strpos($result, 'protected function login()'));
+        assert(strpos($result, 'protected function login()') !== false);
     };
     
     /** isPublic sets the visibility to public */
     yield function () use ($method) {
         $method->isPublic();
         $result = $method->render();
-        assert(strpos($result, 'public function login()'));
+        assert(strpos($result, 'public function login()') !== false);
     };
     
     /** isStatic sets our method to static */
     yield function () use ($method) {
         $method->isStatic();
         $result = $method->render();
-        assert(strpos($result, 'public static function login()'));
+        assert(strpos($result, 'public static function login()') !== false);
     };
     
     /** isFinal sets our method to final */
     yield function () use ($method) {
         $method->isFinal();
         $result = $method->render();
-        assert(strpos($result, 'public final static function login()'));
+        assert(strpos($result, 'public final static function login()') !== false);
     };
     
     /** isAbstract sets our method to abstract */
     yield function () use ($method) {
         $method->isAbstract();
         $result = $method->render();
-        assert(strpos($result, 'public final abstract static function login()'));
+        assert(strpos($result, 'public final abstract static function login()') !== false);
     };
     
     /** setReturnType defines our return type */
     yield function () use ($method) {
         $method->setReturnType('string');
         $result = $method->render();
-        assert(strpos($result, 'public final abstract static function login() : string'));
+        assert(strpos($result, 'public final abstract static function login() : string') !== false);
     };
     
     /** setNullable allows the method to return null instead */
     yield function () use ($method) {
         $method->setNullable(true);
         $result = $method->render();
-        assert(strpos($result, 'public final abstract static function login() :? string'));
+        assert(strpos($result, 'public final abstract static function login() :? string') !== false);
     };
     
     /** setBody lets us define the body of our method */
     yield function () use ($method) {
+        $method->isAbstract(false);
         $method->setBody(<<<EOT
 if ((1 +1) === 2) {
     return 'Hier komt ie sws ja';
@@ -89,8 +90,8 @@ if ((1 +1) === 2) {
 EOT
         );
         $result = $method->render();
-        assert(strpos($result, 'if'));
-        assert(strpos($result, 'else'));
+        assert(strpos($result, 'if') !== false);
+        assert(strpos($result, 'else') !== false);
     };    
     
     /** addArgument lets us set arguments for our method */
@@ -103,14 +104,14 @@ EOT
         $argument = Wrapper::createObject(Codger\Php\Argument::class, $twig, $parameter);
         $method->addArgument($argument);
         $result = $method->render();
-        assert(strpos($result, 'public final abstract static function login(string $user) :? string'));
+        assert(strpos($result, 'public final static function login(string $user) :? string') !== false);
     };
     
     /** initFromClosure can retrieve information from a callable */
     yield function () use ($method) {
-        $callable = function (string $pass) {};
+        $callable = function (string $user, string $pass = null) {};
         $result = $method->initFromClosure($callable)->render();
-        assert(strpos($result, 'public final abstract static function login(string $user, string $pass) :? string'));
+        assert(strpos($result, 'public final static function login(string $user, string $pass = null) :? string') !== false);
     };
 };
 
