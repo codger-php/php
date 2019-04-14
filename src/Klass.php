1a4c920d943b62d98ccc0329f9487ac8b5a00066
Marijn Ophorst <marijn@monomelodies.nl>
2019-04-14 08:15:36 +0200

    moved to trait

--- src/Klass.php
+++ src/Klass.php
@@ -35,26 +35,6 @@ class Klass extends Interfaze
     }
 
     /**
-     * Define a class property. A value of `null` means 'no initial value.
-     *
-     * @param string $name Name of the property. Omit the dollar sign.
-     * @param string|null $value Optional initial value.
-     * @param string $visibility `public`, `protected` or `private`. Defaults to
-     *  `public`.
-     * @param string|null $doccomment Optional doccomment.
-     * @return Codger\Php\Klass
-     */
-    public function defineProperty(string $name, string $value = null, string $visibility = 'public', string $doccomment = null) : Klass
-    {
-        $property = new Property($this->twig, $name, $value, $visibility);
-        if ($doccomment) {
-            $property->setDoccomment($doccomment);
-        }
-        $this->variables->properties[$name] = $property;
-        return $this;
-    }
-
-    /**
      * Add a method. The callback is called with the new `Codger\Php\Method` as
      * its argument for further processing. Optionally, the second parameter can
      * be a closure defining the method signature, the callback moving to 3rd.
