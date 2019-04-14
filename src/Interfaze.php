8ec0ba8e61e34ea3be230835d2b746f2e23ebab1
Marijn Ophorst <marijn@monomelodies.nl>
2019-04-14 08:17:11 +0200

    belong in the interface

--- src/Interfaze.php
+++ src/Interfaze.php
@@ -4,5 +4,28 @@ namespace Codger\Php;
 
 class Interfaze extends Objectesque
 {
+    /**
+     * Set the parent class. To unset, pass an empty string.
+     *
+     * @param string $class
+     * @return Codger\Php\Objectesque
+     */
+    public function extendsClass(string $class) : Objectesque
+    {
+        $this->variables->extends = $class;
+        return $this;
+    }
+
+    /**
+     * Define interfaces to implement.
+     *
+     * @param string ...$interfaces
+     * @return Codger\Php\Objectesque
+     */
+    public function implementsInterfaces(string ...$interfaces) : Objectesque
+    {
+        $this->variables->implements = implode(', ', $interfaces);
+        return $this;
+    }
 }
 
