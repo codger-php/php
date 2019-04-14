fd49cea01ee8825b6c5bdcdf293ffa581837f7f3
Marijn Ophorst <marijn@monomelodies.nl>
2019-04-14 12:54:38 +0200

    set doccomment and template

--- src/Treat.php
+++ src/Treat.php
@@ -2,8 +2,14 @@
 
 namespace Codger\Php;
 
+/**
+ * Recipe to generate traits.
+ */
 class Treat extends Objectesque
 {
     use CanUseTrait;
+
+    /** @var string */
+    protected $template = 'trait.html.twig';
 }
 
