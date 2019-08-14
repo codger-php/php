9add8df0576b4aee6bf3a8b401a2b215c2b689dc
Marijn Ophorst <marijn@monomelodies.nl>
2019-08-14 17:30:38 +0200

    pass twig as constructor param, and rename for clarity

--- tests/treat.php
+++ tests/treat.php
@@ -3,49 +3,49 @@
 use Gentry\Gentry\Wrapper;
 use Codger\Php\Treat;
 
-$klass = Wrapper::createObject(Treat::class);
+$trait = Wrapper::createObject(Treat::class, new Twig_Environment(new Twig_Loader_Filesystem(dirname(__DIR__).'/src')));
 
 /** Test Treat */
-return function () use ($klass) : Generator {
+return function () use ($trait) : Generator {
     /** setNamespace sets a namespace for our trait */
-    yield function () use ($klass) {
-        $klass->setNamespace('Unittest');
-        $result = $klass->render();
+    yield function () use ($trait) {
+        $trait->setNamespace('Unittest');
+        $result = $trait->render();
         assert(strpos($result, 'namespace Unittest'));
     };
     
     /** usesNamespaces lets us use a namespace */
-    yield function () use ($klass) {
-        $klass->usesNamespaces('Tools');
-        $result = $klass->render();
+    yield function () use ($trait) {
+        $trait->usesNamespaces('Tools');
+        $result = $trait->render();
         assert(strpos($result, 'use Tools'));
     };
     
     /** setName lets us name our trait */
-    yield function () use ($klass) {
-        $klass->setName('Technology');
-        $result = $klass->render();
+    yield function () use ($trait) {
+        $trait->setName('Technology');
+        $result = $trait->render();
         assert(strpos($result, 'trait Technology'));
     };
     
     /** usesTraits lets us define traits to use */
-    yield function () use ($klass) {
-        $klass->usesTraits('PasswordGenerator', 'LoginScript');
-        $result = $klass->render();
+    yield function () use ($trait) {
+        $trait->usesTraits('PasswordGenerator', 'LoginScript');
+        $result = $trait->render();
         assert(strpos($result, 'use PasswordGenerator'));
         assert(strpos($result, 'use LoginScript'));
     };
     
     /** defineProperty lets us set properties with visibility of our choice */
-    yield function () use ($klass) {
-        $klass->defineProperty('user', null, 'private');
-        $result = $klass->render();
+    yield function () use ($trait) {
+        $trait->defineProperty('user', null, 'private');
+        $result = $trait->render();
         assert(strpos($result, 'private $user'));
     };
     
     /** addMethod lets us create a method and allows us to retrieve it */
-    yield function () use ($klass) {
-        $klass->addMethod('login', function (string $user, string $pass) : bool {}, function ($method) {
+    yield function () use ($trait) {
+        $trait->addMethod('login', function (string $user, string $pass) : bool {}, function ($method) {
             $method->setBody(<<<EOT
         if (\$user === 'test' && \$pass === 'blarps') {
     return true;
@@ -55,7 +55,7 @@ return function () use ($klass) : Generator {
 EOT
             );
         });
-        $result = $klass->getMethod('login')->render();
+        $result = $trait->getMethod('login')->render();
         assert(strpos($result, 'function login(string $user, string $pass) : bool'));
     };
 };
