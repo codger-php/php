31f728c35a0786a5b67d5c3017251951d1321451
Marijn Ophorst <marijn@monomelodies.nl>
2019-08-14 17:31:55 +0200

    fix test description

--- tests/property.php
+++ tests/property.php
@@ -7,8 +7,9 @@ $twig = new Twig_Environment(new Twig_Loader_Filesystem(dirname(__DIR__).'/templ
 
 $property = Wrapper::createObject(Property::class, $twig, 'user', 'danny', 'private');
 
-/** We can create a property with a default value and a visibility we choose */
+/** Tests for properties */
 return function () use ($property) : Generator {
+    /** We can create a property with a default value and a visibility we choose */
     yield function () use ($property) {
         $result = $property->render();
         assert(strpos($result, 'private $user = \'danny\';'));
