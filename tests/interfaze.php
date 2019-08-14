84a2c6845844c9c7c0d4ca47942ee112dba49ae6
Marijn Ophorst <marijn@monomelodies.nl>
2019-08-14 17:30:22 +0200

    pass twig as constructor param, and rename for clarity

--- tests/interfaze.php
+++ tests/interfaze.php
@@ -3,49 +3,49 @@
 use Gentry\Gentry\Wrapper;
 use Codger\Php\Interfaze;
 
-$klass = Wrapper::createObject(Interfaze::class);
+$interface = Wrapper::createObject(Interfaze::class, new Twig_Environment(new Twig_Loader_Filesystem(dirname(__DIR__).'/src')));
 
 /** Test Interfaze */
-return function () use ($klass) : Generator {
+return function () use ($interface) : Generator {
     /** setNamespace sets a namespace for our interface */
-    yield function () use ($klass) {
-        $klass->setNamespace('Unittest');
-        $result = $klass->render();
+    yield function () use ($interface) {
+        $interface->setNamespace('Unittest');
+        $result = $interface->render();
         assert(strpos($result, 'namespace Unittest') !== false);
     };
     
     /** usesNamespaces lets us use a namespace */
-    yield function () use ($klass) {
-        $klass->usesNamespaces('Tools');
-        $result = $klass->render();
+    yield function () use ($interface) {
+        $interface->usesNamespaces('Tools');
+        $result = $interface->render();
         assert(strpos($result, 'use Tools') !== false);
     };
     
     /** setName lets us name our interface */
-    yield function () use ($klass) {
-        $klass->setName('Technology');
-        $result = $klass->render();
+    yield function () use ($interface) {
+        $interface->setName('Technology');
+        $result = $interface->render();
         assert(strpos($result, 'interface Technology') !== false);
     };
     
     /** extendsClass lets our interface extend another */
-    yield function () use ($klass) {
-        $klass->extendsClass('Work');
-        $result = $klass->render();
+    yield function () use ($interface) {
+        $interface->extendsClass('Work');
+        $result = $interface->render();
         assert(strpos($result, 'interface Technology extends Work') !== false);
     };
     
     /** definesConstants lets us set constants */
-    yield function () use ($klass) {
-        $klass->definesConstants(['host' => 'localhost']);
-        $result = $klass->render();
+    yield function () use ($interface) {
+        $interface->definesConstants(['host' => 'localhost']);
+        $result = $interface->render();
         assert(strpos($result, "const host = 'localhost'") !== false);
     };
     
     /** addMethod lets us create a method and allows us to retrieve it */
-    yield function () use ($klass) {
-        $klass->addMethod('login', function (string $user, string $pass) : bool {}, function () {});
-        $result = $klass->getMethod('login')->render();
+    yield function () use ($interface) {
+        $interface->addMethod('login', function (string $user, string $pass) : bool {}, function () {});
+        $result = $interface->getMethod('login')->render();
         assert(strpos($result, 'function login(string $user, string $pass) : bool;') !== false);
     };
 };
