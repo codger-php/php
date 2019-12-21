87ea0414ad9dfc6a296d69a0df91e7def12aef2c
Marijn Ophorst <marijn@monomelodies.nl>
2019-12-21 13:37:59 +0100

    extend a basefunction

--- src/Funktion.php
+++ src/Funktion.php
@@ -2,31 +2,15 @@
 
 namespace Codger\Php;
 
-use Twig_Environment;
-use DomainException;
-use ReflectionFunction;
-
-class Funktion extends Lambda
+class Funktion extends BaseFunction
 {
-    use Doccomment;
-
     /** @var string */
     protected $_template = 'function.html.twig';
-
-    /**
-     * @param array|null $arguments
-     * @param callable $declaration Optional argument and return type
-     *  declaration closure (function can have empty body)
-     */
-    public function __construct(array $arguments = null, callable $declaration = null)
-    {
-        parent::__construct($arguments, $declaration);
-    }
 
     public function __invoke(string $name) : void
     {
         $this->set('name', $name);
-        parent::__invoke();
+        $this->output("$name.php");
     }
 }
 
