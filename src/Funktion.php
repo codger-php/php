afe9a5e55c547c4a19fc26378afcf4eb905a4af5
Marijn Ophorst <marijn@monomelodies.nl>
2019-12-21 09:53:49 +0100

    modernize as command

--- src/Funktion.php
+++ src/Funktion.php
@@ -14,15 +14,19 @@ class Funktion extends Lambda
     protected $_template = 'function.html.twig';
 
     /**
-     * @param string $name Name of the function
-     * @param Twig_Environment $twig Optional Twig environment
+     * @param array|null $arguments
      * @param callable $declaration Optional argument and return type
      *  declaration closure (function can have empty body)
      */
-    public function __construct(string $name, Twig_Environment $twig = null, callable $declaration = null)
+    public function __construct(array $arguments = null, callable $declaration = null)
     {
-        parent::__construct($twig, $declaration);
-        $this->_variables->name = $name;
+        parent::__construct($arguments, $declaration);
+    }
+
+    public function __invoke(string $name) : void
+    {
+        $this->set('name', $name);
+        parent::__invoke();
     }
 }
 
