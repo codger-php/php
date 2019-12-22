1faea6c041f1a09f3c4c11811c945ea586c77068
Marijn Ophorst <marijn@monomelodies.nl>
2019-12-22 09:56:20 +0100

    make properties more versatile

--- src/Property.php
+++ src/Property.php
@@ -4,6 +4,7 @@ namespace Codger\Php;
 
 use Twig_Environment;
 use ReflectionParameter;
+use DomainException;
 
 class Property extends Recipe
 {
@@ -15,6 +16,9 @@ class Property extends Recipe
 
     /** @var string */
     public $visibility;
+
+    /** @var bool */
+    public $static = false;
 
     /** @var string */
     protected $_template = 'property.html.twig';
@@ -29,7 +33,48 @@ class Property extends Recipe
         $this->persistOptionsToTwig('default');
     }
 
-    public function __invoke(string $name)
+    /**
+     * @param string $visibility
+     * @return Codger\Php\Property
+     * @throws DomainException if $visibility is not public, protected or
+     *  private.
+     */
+    public function setVisibility(string $visibility) : Property
+    {
+        if (!in_array($visibility(['public', 'protected', 'private']))) {
+            throw new DomainException("Visibility must be public, protected or private.");
+        }
+        return $this->set('visibility', $visibility);
+    }
+
+    /**
+     * @param mixed $default The initial value of the property. Pass null to
+     *  remove the initial value completely.
+     * @return Codger\Php\Property
+     */
+    public function setDetault($default = null) : Property
+    {
+        if (isset($default)) {
+            return $this->set('default', $this->quote($default));
+        } else {
+            return $this->set('default', null);
+        }
+    }
+
+    /**
+     * @param bool $static
+     * @return Codger\Php\Property
+     */
+    public function isStatic(bool $static = true) : Property
+    {
+        return $this->set('static', $static);
+    }
+
+    /**
+     * @param string $name
+     * @return void
+     */
+    public function __invoke(string $name) : void
     {
         $this->set('name', $name);
         $this->output("$name.php");
