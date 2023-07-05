29d567521a359f66b09bcf6ceb0a0e3cf7a944be
Marijn Ophorst <marijn@sensimedia.nl>
2023-07-05 14:59:34 +0200

    add type hint to test, and hide output

--- tests/property.php
+++ tests/property.php
@@ -1,8 +1,11 @@
 <?php
 
 use Codger\Php\Property;
+use Codger\Generate\{ Recipe, FakeInOut };
 
-$property = new Property(['user', '--default', 'danny', '--visibility', 'private']);
+Recipe::setInOut(new FakeInOut);
+
+$property = new Property(['user', '--default', 'danny', '--visibility', 'private', '--type', 'string']);
 $property->execute();
 
 /** Tests for properties */
@@ -10,7 +13,7 @@ return function () use ($property) : Generator {
     /** We can create a property with a default value and a visibility we choose */
     yield function () use ($property) {
         $result = $property->render();
-        assert(strpos($result, 'private $user = \'danny\';'));
+        assert(strpos($result, 'private string $user = \'danny\';'));
     };
 };
 
